40edf55aab224ec7faca9795dd43f1882417ea7d
rayaniselim <rayaniselim@outlook.com>
2023-02-08 22:31:30 -0400

    feat colors theme dark

--- design_system/lib/ui/styles/colors_app.dart
+++ design_system/lib/ui/styles/colors_app.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
 class ColorsApp {
-  // static Color get primary => const Color(0xff);
-  // static Color get secondary => const Color(0xff);
+  /// theme dark
+
+  static Color get primary => const Color(0xff1B202D);
+  static Color get secondary => const Color(0xffFFFFFF);
+  static Color get cardChatsColor => const Color(0xff292F3F);
+  static Color get myMessageContainerColor => const Color(0xff7A8194);
+  static Color get yourMessageContainerColor => const Color(0xff373E4E);
+  static Color get writeBoxColor => const Color(0xff3D4354);
+  static Color get chatButtonBackgroundColor => const Color(0xff9398A7);
+  static Color get secondaryTextColor => const Color(0xffB3B9C9);
 }
